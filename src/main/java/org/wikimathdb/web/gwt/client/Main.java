14fc76ef3ed40d24486733d53286d04a6963d604
dlazerka <dlazerka@c9897767-4c3c-0410-8b16-9d06ef18cdb1>
2007-11-01 16:00:37 +0000

    Added WikiMathDB Idea Description.

--- src/main/java/org/wikimathdb/web/gwt/client/Main.java
+++ src/main/java/org/wikimathdb/web/gwt/client/Main.java
@@ -1,12 +1,15 @@
 package org.wikimathdb.web.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Main implements EntryPoint {
 	public void onModuleLoad() {
 		RootPanel rootPanel = RootPanel.get();
-		rootPanel.add(new Label("Hello World!"));
+		DockPanel dockPanel = new DockPanel();
+		rootPanel.add(dockPanel);
+		dockPanel.add(new Label("Hello World!"), DockPanel.NORTH);
 	}
 }
